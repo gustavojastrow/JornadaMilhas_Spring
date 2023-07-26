207d66b18875710886f64ac42edd23ad04bbe09d
Gustavo Jastrow <jaastrow@hotmail.com>
2023-07-25 22:40:21 -0300

    Response Entity

--- src/main/java/com/jast/jornada/milhas/controller/DepoimentoController.java
+++ src/main/java/com/jast/jornada/milhas/controller/DepoimentoController.java
@@ -1,9 +1,11 @@
 package com.jast.jornada.milhas.controller;
 
-import java.util.List;
+import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.jast.jornada.milhas.domain.depoimento.DadosCadastro;
 import com.jast.jornada.milhas.domain.depoimento.Depoimento;
@@ -28,27 +31,41 @@ public class DepoimentoController {
 
     @Transactional
     @PostMapping
-    public void cadastrarDepoimento(@RequestBody DadosCadastro dados){
-        depoimentoRepository.save(new Depoimento(dados));
+    public ResponseEntity<Depoimento> cadastrarDepoimento(@RequestBody DadosCadastro dados, UriComponentsBuilder uriBuilder) throws IOException{
+        var depoimento = new Depoimento(dados);
+        depoimentoRepository.save(depoimento);
+        var uri = uriBuilder.path("/depoimentos/{id}").buildAndExpand(depoimento.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(depoimento);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ListagemDepoimento> listarPorId(@PathVariable Long id){
+        var depoimentoId = depoimentoRepository.getReferenceById(id);
+        return ResponseEntity.ok(new ListagemDepoimento(depoimentoId));
     }
 
     @GetMapping
-    public List<ListagemDepoimento> listar(){
-        return depoimentoRepository.findAll().stream().map(ListagemDepoimento::new).toList();
+    public ResponseEntity<Object> listar(@PageableDefault(size=10) Pageable paginacao){
+        var depoimento = depoimentoRepository.findAll(paginacao).map(ListagemDepoimento::new);
+        return ResponseEntity.ok(depoimento);
     }
-
 
     @Transactional
     @PutMapping
-    public void atualizarDepoimento(@RequestBody ListagemDepoimento dados){
+    public ResponseEntity<ListagemDepoimento> atualizarDepoimento(@RequestBody ListagemDepoimento dados){
         var depoimento = depoimentoRepository.getReferenceById(dados.id());
         depoimento.atualizarDepoimento(dados);
+
+        return ResponseEntity.ok(new ListagemDepoimento(depoimento));
     }
 
 
     @Transactional
     @DeleteMapping("/{id}")
-    public void deletar(@PathVariable Long id){
+    public ResponseEntity<Object> deletar(@PathVariable Long id){
         depoimentoRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+        
     }
 }
